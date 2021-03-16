7aa5874a1f260d88fe023839af06eb64bd069cf9
jwendel <jwendel@butler.edu>
2021-03-16 15:04:16 -0400

    Just edited some comments

--- workflowLoginForm/RegisterForm.cs
+++ workflowLoginForm/RegisterForm.cs
@@ -23,9 +23,10 @@ namespace workflowLoginForm
         // Event handler for Clear button click
         private void clearBtn_Click(object sender, EventArgs e)
         {
-            passwordTxt.Text = String.Empty; // Empty password text
-            usernameTxt.Text = String.Empty; // Empty username text
-            boxOccupation.Text = String.Empty; // Empty user job text
+            // Clear text fields
+            passwordTxt.Text = String.Empty; 
+            usernameTxt.Text = String.Empty; 
+            boxOccupation.Text = String.Empty; 
         }
 
         // Event handler for Register button click
@@ -34,24 +35,25 @@ namespace workflowLoginForm
             // FIX PATH
             if(passwordTxt.Text == confirmpasswordtxt.Text)
             {
-                //C: \Users\jwend\source\Repo\workflowLoginForm\UserLoginData.mdf"
-                string con = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\ian\Source\Repos\WORK-FLOW\workflowLoginForm\UserLoginData.mdf"; // Connection string
-                SqlConnection connectionOne = new SqlConnection(con); // Creating an instance of the database
+                    //C: \Users\jwend\source\Repo\workflowLoginForm\UserLoginData.mdf"
+                    string con = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\ian\Source\Repos\WORK-FLOW\workflowLoginForm\UserLoginData.mdf"; // Connection string
+                    SqlConnection connectionOne = new SqlConnection(con); // Creating an instance of the database
 
-                SqlCommand one = new SqlCommand("Insert into AuthorizedUsers(username, userpassword, userjob) VALUES (@username, @password, @job);", connectionOne); // Create a SQL command that takes user input -- username, password, and job -- and inputs into the database
-                one.Parameters.AddWithValue("@username", usernameTxt.Text); // Database uses the username text and puts it into the username column
-                one.Parameters.AddWithValue("@password", passwordTxt.Text); // Database uses the password text and puts it into the userpassword column
-                one.Parameters.AddWithValue("@job", boxOccupation.Text); // Database uses the job text and puts it into the userjob column
+                    // Create a SQL command that takes user input -- username, password, and job -- and inputs into the database
+                    SqlCommand one = new SqlCommand("Insert into AuthorizedUsers(username, userpassword, userjob) VALUES (@username, @password, @job);", connectionOne);
+                    one.Parameters.AddWithValue("@username", usernameTxt.Text); 
+                    one.Parameters.AddWithValue("@password", passwordTxt.Text); 
+                    one.Parameters.AddWithValue("@job", boxOccupation.Text); 
 
-                connectionOne.Open(); // Open the sql connection
-                one.ExecuteNonQuery(); // Execute the sql command
-                connectionOne.Close(); // Close the sql connection
-                MessageBox.Show("Sucessfully Registered!");
+                    connectionOne.Open(); // Open the sql connection
+                    one.ExecuteNonQuery(); // Execute the sql command
+                    connectionOne.Close(); // Close the sql connection
+                    MessageBox.Show("Sucessfully Registered!");
 
-                // Clear values for other users
-                usernameTxt.Clear();
-                passwordTxt.Clear();
-                boxOccupation.Text = String.Empty;
+                    // Clear values for other users
+                    usernameTxt.Clear();
+                    passwordTxt.Clear();
+                    boxOccupation.Text = String.Empty;
             }
             else
             {
