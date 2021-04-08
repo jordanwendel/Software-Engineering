8995582edb59ea12eb639280515d9762b47036db
Justin Andrea <jandrea@butler.edu>
2021-04-07 21:28:42 -0400

    Housekeeping Product Manager page

--- workflowLoginForm/ProductManagerForm.Designer.cs
+++ workflowLoginForm/ProductManagerForm.Designer.cs
@@ -326,11 +326,12 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 355);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 352);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 17);
+            this.label2.Size = new System.Drawing.Size(269, 25);
             this.label2.TabIndex = 37;
-            this.label2.Text = "Select one to filter by";
+            this.label2.Text = "Select One Option to Filter By";
             // 
             // radBtnQuality
             // 
@@ -386,6 +387,7 @@
             this.cBoxQuality.Name = "cBoxQuality";
             this.cBoxQuality.Size = new System.Drawing.Size(163, 24);
             this.cBoxQuality.TabIndex = 42;
+            this.cBoxQuality.Text = "Click to expand..";
             // 
             // txtTest
             // 
@@ -432,6 +434,7 @@
             this.cBoxLocation.Name = "cBoxLocation";
             this.cBoxLocation.Size = new System.Drawing.Size(163, 24);
             this.cBoxLocation.TabIndex = 47;
+            this.cBoxLocation.Text = "Click to expand..";
             // 
             // ProductManagerForm
             // 
